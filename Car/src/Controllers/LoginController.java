7f92d46153c3b4a474e5364ad7daaa2be5a2b039
John Jorjill <orgil9506@gmail.com>
2021-01-04 15:42:49 -0600

    Added LoginController controller

--- Car/src/Controllers/LoginController.java
+++ Car/src/Controllers/LoginController.java
@@ -1,5 +1,61 @@
 package Controllers;
 
-public class LoginController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.jfoenix.controls.*;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class LoginController implements Initializable {
+	
+    @FXML
+    private JFXTextField username;
+
+    @FXML
+    private JFXPasswordField password;
+
+    @FXML
+    private JFXButton signup;
+
+    @FXML
+    private JFXCheckBox checkbox;
+
+    @FXML
+    private JFXButton login;
+
+    @FXML
+    private JFXButton forgot;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@FXML
+	public void loginAction(ActionEvent e) {
+		System.out.println("Login Successful!");
+	}
+	
+	@FXML
+	public void signUp(ActionEvent e1) throws IOException {
+		login.getScene().getWindow().hide();
+		
+		Stage signup = new Stage();
+		Parent root = FXMLLoader.load(getClass().getResource("/FXML/SignUP.fxml"));
+		Scene scene = new Scene(root);
+		signup.setScene(scene);
+		signup.show();
+		signup.setResizable(false);
+		
+	}
 
 }
